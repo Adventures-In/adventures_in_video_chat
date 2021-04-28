7aac06ec75d680891218caff9ba381c88ebdeaab
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-28 13:40:28 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# adventures_in_video_chat
+
+
+
+[Notes](https://docs.google.com/document/d/1lbLOlqVEiXH73_k9RAhEa6OuNnIFHjJixLyGKolbjKg/edit?usp=sharing)
---
+++ android/app/src/main/kotlin/co/enspyr/adventures_in_video_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.enspyr.adventures_in_video_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
